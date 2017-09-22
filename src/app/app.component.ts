d5665d552f078a764961d06f9fcbf7c6abf429a7
Edwin ter Horst <contact@nrzonline.nl>
2017-09-22 12:51:26 +0200

    Removed background bars html/css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,6 @@ export class AppComponent implements AfterViewInit, OnInit, OnDestroy {
     private _subscriptions: Subscription[] = [];
 
     constructor(private translate: TranslateService,
-                private ngZone: NgZone,
                 private router: Router){
         this.translate.setDefaultLang('en');
         this.translate.use('en');
@@ -27,8 +26,6 @@ export class AppComponent implements AfterViewInit, OnInit, OnDestroy {
 
     public ngAfterViewInit(){
         this.rememberScrollPositionOnRouteChange();
-        this.adjustBackgroundPosition();
-        this.adjustBackgroundPositionOnWindowResize();
     }
     
     public ngOnDestroy() {
@@ -50,33 +47,5 @@ export class AppComponent implements AfterViewInit, OnInit, OnDestroy {
                 }
             })
         );
-    }
-
-    private adjustBackgroundPositionOnWindowResize(){
-        window.onresize = () => {
-            this.adjustBackgroundPosition();
-        }
-    }
-
-    private adjustBackgroundPosition(){
-        this.ngZone.run(() => {
-            let width = window.innerWidth;
-
-            if(width >= 800) {
-                let bgFirstRow = document.getElementById('bg-first-row');
-                let bgSecondRow = document.getElementById('bg-second-row');
-                let bgThirdRow = document.getElementById('bg-third-row');
-                let bgFourthRow = document.getElementById('bg-fourth-row');
-                let bgFifthRow = document.getElementById('bg-fifth-row');
-                let bgSixthRow = document.getElementById('bg-sixth-row');
-
-                bgFirstRow.style.left = -1350 + (width / 2) + 'px';
-                bgSecondRow.style.left = -2650 + (width / 2) + 'px';
-                bgThirdRow.style.left = -2750 + (width / 2) + 'px';
-                bgFourthRow.style.left = -1250 + (width / 2) + 'px';
-                bgFifthRow.style.left = -2375 + (width / 2) + 'px';
-                bgSixthRow.style.left = -1625 + (width / 2) + 'px';
-            }
-        });
     }
 }
